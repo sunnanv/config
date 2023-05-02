f6b54052d7d2dba4454600ac0383777aec36e0a2
Johannes Sunnanvader <johannes.sunnan@gmail.com>
2023-05-02 12:17:58 +0200

    Upgrade null-ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,11 +4,10 @@ function M.setup()
     require("mason-null-ls").setup({
         automatic_installation = false,
         automatic_setup = true,
+        handlers = {}
     })
     require("null-ls").setup(
     )
-
-    require 'mason-null-ls'.setup_handlers()
 end
 
 return M
