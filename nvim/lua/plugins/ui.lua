0eb7fa39485c393c1c83f5189a7d064e1da01978
Johannes Sunnanvader <johannes.sunnan@gmail.com>
2024-10-23 18:13:00 +0200

    Add dashboard-nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -93,4 +93,44 @@ return {
             })
         end,
     },
+    {
+        'nvimdev/dashboard-nvim',
+        config = function()
+            require('dashboard').setup {
+                theme = 'hyper',
+                config = {
+                    mru = { limit = 10, label = '', cwd_only = true },
+                    project = { enable = false },
+                    week_header = {
+                        enable = true,
+                    },
+                    shortcut = {
+                        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                        {
+                            icon = ' ',
+                            icon_hl = '@variable',
+                            desc = 'Files',
+                            group = 'Label',
+                            action = 'Telescope find_files',
+                            key = 'f',
+                        },
+                        {
+                            desc = ' Apps',
+                            group = 'DiagnosticHint',
+                            action = 'Telescope app',
+                            key = 'a',
+                        },
+                        {
+                            desc = ' dotfiles',
+                            group = 'Number',
+                            action = 'Telescope dotfiles',
+                            key = 'd',
+                        },
+                    },
+                },
+
+            }
+        end,
+        dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+    }
 }
