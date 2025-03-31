5a7210a7ff3c6ef15ba5d811a42909870767b563
Johannes Sunnanvader <johannes.sunnan@gmail.com>
2025-03-31 22:13:52 +0200

    Add nvim-ufo

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -113,5 +113,57 @@ return {
                 desc = "Quickfix List (Trouble)",
             },
         },
+    },
+    {
+        'kevinhwang91/nvim-ufo',
+        dependencies = { 'kevinhwang91/promise-async' },
+        keys = {
+            {
+                "zR",
+                function()
+                    require('ufo').openAllFolds()
+                end,
+                desc = "Open all folds (ufo)",
+            },
+            {
+                "zM",
+                function()
+                    require('ufo').closeAllFolds()
+                end,
+                desc = "Close all folds (ufo)",
+            },
+            {
+                "zK",
+                function()
+                    local winid = require('ufo').peekFoldedLinesUnderCursor()
+                    if not winid then
+                        vim.lsp.buf.hover()
+                    end
+                end,
+                desc = "Peek folded lines under cursor (ufo)",
+                remap = true,
+            }
+        },
+        event = "VeryLazy",
+        init = function()
+            vim.o.foldcolumn = '1' -- '0' is not bad
+            vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true
+            }
+            local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
+            for _, ls in ipairs(language_servers) do
+                require('lspconfig')[ls].setup({
+                    capabilities = capabilities
+                    -- you can add other fields for setting up lsp server in this table
+                })
+            end
+            require('ufo').setup()
+        end
     }
 }
